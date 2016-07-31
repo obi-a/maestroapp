a82fa43198f733ecd7e77e6d47174a55b550d6f3
Obi Akubue <obioraakubue@yahoo.com>
2016-07-31 06:13:23 -0400

    make monitor creation job idempotent

--- app/jobs/monitor_creation_job.rb
+++ app/jobs/monitor_creation_job.rb
@@ -6,31 +6,33 @@ class MonitorCreationJob < ActiveJob::Base
     client = RagiosMonitor.client
     ActiveRecord::Base.connection_pool.with_connection do
       m = RagiosMonitor.find(monitor_id)
-      options = {
-        monitor: m.title,
-        url: m.url,
-        via: "webhook_notifier",
-        webhook_url: webhook_url,
-        user: m.user.email,
-        plugin: m.monitor_type,
-        every: "#{m.hours}h#{m.minutes}m"
-      }
-      options[:browser] = "firefox headless"
-      options[:exists?] = m.code
-      response = client.create(options)
+      unless m.status > 0
+        options = {
+          monitor: m.title,
+          url: m.url,
+          via: "webhook_notifier",
+          webhook_url: webhook_url,
+          user: m.user.email,
+          plugin: m.monitor_type,
+          every: "#{m.hours}h#{m.minutes}m"
+        }
+        options[:browser] = "firefox headless"
+        options[:exists?] = m.code
+        response = client.create(options)
 
-      if response["error"]
-        m.update_attributes(
-          comment: response["error"],
-          status: RagiosMonitor.status(:cannot_create)
-        )
-      else
-        m.update_attributes(
-          comment: "Successfully created",
-          status: RagiosMonitor.status(:active),
-          ragiosid: response[:_id],
-          monitor_json: response.to_json
-        )
+        if response["error"]
+          m.update_attributes(
+            comment: response["error"],
+            status: RagiosMonitor.status(:cannot_create)
+          )
+        else
+          m.update_attributes(
+            comment: "Successfully created",
+            status: RagiosMonitor.status(:active),
+            ragiosid: response[:_id],
+            monitor_json: response.to_json
+          )
+        end
       end
     end
   end
